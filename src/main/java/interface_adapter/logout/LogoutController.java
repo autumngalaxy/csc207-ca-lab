af486c6a211e1ace914660db74d87b67d41fec5b
Angelica Pozzuoli <angelicapozzuoli@gmail.com>
2025-10-20 10:34:55 -0400

    Implement LogoutController

--- src/main/java/interface_adapter/logout/LogoutController.java
+++ src/main/java/interface_adapter/logout/LogoutController.java
@@ -12,7 +12,6 @@ public class LogoutController {
     public LogoutController(LogoutInputBoundary logoutUseCaseInteractor) {
         this.logoutUseCaseInteractor = logoutUseCaseInteractor;
     }
-
     /**
      * Executes the Logout Use Case.
      */
